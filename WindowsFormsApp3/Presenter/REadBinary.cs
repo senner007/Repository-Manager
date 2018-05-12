764f19b2961781a6b97aa414e139b5434a2892be
senner007 <nielshtg@hotmail.com>
2018-05-12 23:21:24 +0200

    clean up binary

--- WindowsFormsApp3/Presenter/REadBinary.cs
+++ WindowsFormsApp3/Presenter/REadBinary.cs
@@ -14,28 +14,21 @@ namespace Manager.Presenter
         private static CultureInfo culture = CultureInfo.CurrentCulture; // TODO : performance ?
         public static IEnumerable<T> ListBinary<T>(List<T> list, string val) where T : IPerson
         {
-            //  Console.WriteLine(list[0]);
             val = val.ToLower(culture);
+            int valLength = val.Length;
             
             var listCount = list.Count();
        
-            int index = BinaryCompareStrings(list, val, 0, listCount);
+            int index = BinaryCompareStrings(list, 0, listCount);
 
             List<T> newList = new List<T>();
             bool condition = true;
             if (index == -1) return newList;
-            Console.WriteLine(index);
             while (condition && index < listCount)
             {
-              //  Console.WriteLine(list[index].LastName);
-             //   Console.WriteLine(val.Length);
-                int length = val.Length;
                 T item = list[index];
-                if (val.Length > item.LastName.Length)
-                {
-                    length = item.LastName.Length;
-                }
-
+                int length = valLength > item.LastName.Length ? item.LastName.Length : valLength;
+        
                 if (item.LastName.Substring(0, length).ToLower(culture) == val) // TODO : korrekt?
                 {
                     newList.Add(item);
@@ -48,27 +41,18 @@ namespace Manager.Presenter
             }
             return newList;
 
-
-            int BinaryCompareStrings<T>(List<T> a, string mystring, int left, int right) where T : IPerson
+            int BinaryCompareStrings<T>(List<T> a,int left, int right) where T : IPerson
             {
                 if (left == right)
                 {
-                    // Console.WriteLine(a[left].LastName.StartsWith(mystring));
-                    //   Console.WriteLine(a[left].LastName);
-                    return left >= 0 && left < a.Count && a[left].LastName.ToLower(culture).StartsWith(mystring) ? left : -1;
+                    return left >= 0 && left < a.Count && a[left].LastName.ToLower(culture).StartsWith(val) ? left : -1;
                 }
                 int mid = (right + left) / 2;
 
-                int mylength = mystring.Length;
-                if (mylength > a[mid].LastName.Length)
-                {
-                    mylength = a[mid].LastName.Length;
+                int mylength = valLength > a[mid].LastName.Length ? a[mid].LastName.Length : valLength;
 
-                }
-                return (string.Compare(mystring, a[mid].LastName.ToLower(culture).Substring(0, mylength)) == 1) ? BinaryCompareStrings(a, mystring, mid + 1, right) : BinaryCompareStrings(a, mystring, left, mid);
+                return (string.Compare(val, a[mid].LastName.ToLower(culture).Substring(0, mylength)) == 1) ? BinaryCompareStrings(a, mid + 1, right) : BinaryCompareStrings(a, left, mid);
             }
         }
-        
-
     }
 }
