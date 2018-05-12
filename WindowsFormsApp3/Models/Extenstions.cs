70c6227302b9997b20401b35780e6fb1adb224ab
senner007 <nielshtg@hotmail.com>
2018-05-12 22:13:12 +0200

    Extentions class

---
+++ WindowsFormsApp3/Models/Extenstions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Manager.Models
+{
+    public static class Extentions
+    {
+        // extenstion metode der indsætter objekted på den rigtige plads i stedet for at sortere hele listen
+        //(O(n)) TODO: kan forbedres med binær ?
+        public static void AddSorted<T>(this IList<T> list, T item, IComparer<T> comparer = null)
+        {
+            if (comparer == null)
+                comparer = Comparer<T>.Default;
+
+            int i = 0;
+            while (i < list.Count && comparer.Compare(list[i], item) < 0)
+                i++;
+
+            list.Insert(i, item);
+        }
+    }
+}
