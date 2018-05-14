028982076f3576aeae00c4bff6bf047cb875f00e
senner007 <nielshtg@hotmail.com>
2018-05-14 19:19:34 +0200

    addSorted comparer simplified

--- WindowsFormsApp3/Models/Extenstions.cs
+++ WindowsFormsApp3/Models/Extenstions.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Specialized;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,16 +11,15 @@ namespace Manager.Models
     {
         // extenstion metode der indsætter objekted på den rigtige plads i stedet for at sortere hele listen
         //(O(n)) TODO: kan forbedres med binær ?
-        public static void AddSorted<T>(this IList<T> list, T item, IComparer<T> comparer = null)
+        public static void AddSorted<T>(this IList<T> list, T item)
         {
-            if (comparer == null)
-                comparer = Comparer<T>.Default;
-
+            Comparer<T> comparer = Comparer<T>.Default;
             int i = 0;
             while (i < list.Count && comparer.Compare(list[i], item) < 0)
                 i++;
 
             list.Insert(i, item);
         }
+
     }
 }
