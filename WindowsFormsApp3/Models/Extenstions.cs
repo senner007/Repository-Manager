913b3547ba8a9177bb11ddd98771e668eef81b7b
senner007 <nielshtg@hotmail.com>
2018-05-16 19:25:45 +0200

    simplify extension

--- WindowsFormsApp3/Models/Extenstions.cs
+++ WindowsFormsApp3/Models/Extenstions.cs
@@ -11,14 +11,12 @@ namespace Manager.Models
     {
         // extenstion metode der indsætter objekted på den rigtige plads i stedet for at sortere hele listen
         //(O(n)) 
-        public static void AddSortedDict<T>(this OrderedDictionary dict, uint tlf, T item) where T : IPerson
+        public static void AddSortedDict(this OrderedDictionary dict, uint tlf, IPerson item)
         {
             Comparer<IPerson> comparer = Comparer<IPerson>.Default;
-            IEnumerable<IPerson> coll = dict.Values.OfType<IPerson>();
 
-            int i = 0;
-
-            while (i < dict.Count && comparer.Compare(coll.ToList()[i], item) < 0)
+            int i = 0; 
+            while (i < dict.Count && comparer.Compare(dict.Values.OfType<IPerson>().ToList()[i], item) < 0)
                 i++;
 
             dict.Insert(i, tlf, item);
